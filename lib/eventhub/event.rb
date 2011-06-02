965ce3bd7d6be270e6483de96a78c09c555f881d
Marcin Bunsch <marcin@applicake.com>
2011-06-02 22:39:16 +0200

    Added message to uuid generation

--- lib/eventhub/event.rb
+++ lib/eventhub/event.rb
@@ -1,13 +1,13 @@
 require 'digest/sha1'
 
 class EventHub::Event
-  
+
   attr_accessor :uuid, :kind, :user, :repo, :time, :gravatar, :message
   def initialize(attributes = {})
     [:kind, :user, :repo, :time, :gravatar, :message].each do |attribute|
       self.send("#{attribute}=", attributes[attribute])
     end
-    self.uuid = Digest::SHA1.hexdigest("#{kind}-#{user}-#{repo}-#{time}")
+    self.uuid = Digest::SHA1.hexdigest("#{kind}-#{user}-#{repo}-#{time}-#{message}")
   end
 
 end
